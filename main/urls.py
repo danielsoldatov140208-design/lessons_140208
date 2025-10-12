f0d7435dcb110d771e19b545c0920374cf8b5683
KapitanZol <danielsoldatov140208@gmail.com>
2025-10-12 21:44:48 +0500

    lessons 13

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views  # импортируем views из той же папки
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('hello/', views.HelloView.as_view(), name='hello')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
-
+from django.views import View
 def home(request):
-    return HttpResponse("Привет! Это главная страница Django-проекта.")
+    return HttpResponse("главная страница моего сайта")
 
 def about(request):
     return HttpResponse("Обо мне")
+
+def contact(request):
+    return HttpResponse("свяжитесь со мной")
+
+class HelloView(View):
+    def get(self, request):
+        name = request.GET.get("name", "гость")
+        return HttpResponse(f"Привет, {name}!")
