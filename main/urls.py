9bcd0bf7db078c2d774751b39b51c2cb2067ab28
KapitanZol <danielsoldatov140208@gmail.com>
2025-10-13 21:57:41 +0500

    lessons 15

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views  # импортируем views из той же папки
 
 urlpatterns = [
-    path('', views.base, name='home'),
+    path('s', views.base, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('hello/', views.HelloView.as_view(), name='hello'),
     path('articles/', views.articles_view, name='articles'),
-    path('news/', views.news_view, name='news'),
+    path('', views.news_view, name='news'),
     path('home/', views.home, name='home'),
 ]
