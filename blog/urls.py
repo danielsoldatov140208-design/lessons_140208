6e17de4b5f261da9e68a327167464b26a3c7ddd2
danielsoldatov140208-design <danielsoldatov140208@gmail.com>
2025-10-21 16:41:11 +0500

    Delete blog directory

--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.blog_home, name='blog_home'),
-]
--- blog/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def blog_home(request):
-    return HttpResponse("Это блог-раздел моего сайта")
