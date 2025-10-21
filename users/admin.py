0e8d8161935009d5bacd4cb83b03f0988ae53436
danielsoldatov140208-design <danielsoldatov140208@gmail.com>
2025-10-21 16:43:06 +0500

    Delete users directory

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Article
-
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-   list_display = ("id", "title", "author", "created_at")
-   search_fields = ("title", "author")
