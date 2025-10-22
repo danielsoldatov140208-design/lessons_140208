3f51717fc71af21f8d231ab72d73d75e115181d4
Daniel Soldatov <danielsoldatov140208@gmail.com>
2025-10-22 16:01:01 +0500

    урок27

--- myfirstsite/blog/admin.py
+++ myfirstsite/blog/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Article, Author   , Tag       
+
+class ArticleInline(admin.TabularInline):
+    model = Article
+    extra = 1
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email")
+    search_fields = ("name", "email")
+    inlines = [ArticleInline]
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author")
+    search_fields = ("title", "content")
+    list_filter = ("author",)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")  # чтобы видно было ID и название
+    search_fields = ("name",)  
--- myfirstsite/blog/models.py
+++ myfirstsite/blog/models.py
@@ -6,11 +6,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+   name = models.CharField(max_length=30, unique=True)
 
+def __str__(self):
+       return self.name
+   
 class Article(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     content = models.TextField(verbose_name="Содержание")
     author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name="Автор")
+    tags = models.ManyToManyField(
+    'Tag',
+    blank=True,
+    related_name='articles',
+    verbose_name='Теги'
+)
+
 
     def __str__(self):
         return self.title
@@ -18,3 +30,6 @@ class Article(models.Model):
 class Author(models.Model):
     email = models.EmailField(verbose_name="Электронная почта автора")  
     name = models.CharField(max_length=100, verbose_name="Имя автора")
+
+    def __str__(self):
+        return self.name    
