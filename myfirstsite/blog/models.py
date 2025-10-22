e11cb0dc33497a39c4d9434827c3973762fb6d3b
Daniel Soldatov <danielsoldatov140208@gmail.com>
2025-10-22 15:01:56 +0500

    задание 26

--- myfirstsite/blog/models.py
+++ myfirstsite/blog/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
-
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=100, verbose_name="Название категории")
 
     def __str__(self):
         return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержание")
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name="Автор")
+
+    def __str__(self):
+        return self.title
+    
+class Author(models.Model):
+    email = models.EmailField(verbose_name="Электронная почта автора")  
+    name = models.CharField(max_length=100, verbose_name="Имя автора")
